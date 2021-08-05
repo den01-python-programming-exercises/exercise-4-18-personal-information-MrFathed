6bceb2a3e0f6d389e87b949d4f916f21d7d6f0e8
Justin Turner <jmturner638@gmail.com>
2021-08-05 06:52:04 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,21 @@
+from personal_information import PersonalInformation
+
 def main():
     #write your code below this line
+    people = []
+
+    while True:
+        first_name = input("First name:")
+
+        if not first_name:
+            break
+
+        last_name  = input("Last name:")
+        id_num = input("Identification number:")
+        people.append(PersonalInformation(first_name, last_name, id_num))
+
+    for person in people:
+        print(person.get_first_name(), person.get_last_name())
 
 if __name__ == '__main__':
     main()
